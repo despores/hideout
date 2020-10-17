89dc2d9af8a1b4259534ae1206e75bfe3bddb57f
ernest_kot <ersakhabutdinov@edu.hse.ru>
2020-10-17 10:32:11 +0300

    reworked

--- FileManager/FileManager/Program.cs
+++ FileManager/FileManager/Program.cs
@@ -5,22 +5,13 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Добро пожаловать в \"Файловый менеджер\".");
+        Console.WriteLine("File manager.");
+        Console.WriteLine("Type `help' to find out more about functions");
         while (true)
         {
-            string userInput = StartMenu();
-            if (userInput == "help")
-            {
-                HelpText();
-            }
-            if(userInput == "1")
-            {
-                WriteAllDrives();
-            }
-            if(userInput == "7")
-            {
-                MoveFile();
-            }
+            Console.Write(">>>");
+            ReadOrExit(out string userInput);
+            CommandQuery(userInput);
         }
     }
 }
