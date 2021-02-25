1331cfe9bc8337e788cf6a50e6db42774dc51de7
despores <ernest.sakhabutdinov@mail.ru>
2021-02-26 01:25:48 +0500

    Начальный возврат модуля classwork

---
+++ classwork/Company.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Company
+{
+    private readonly List<TeamLead> teamLeads;
+
+    public Company(int teamLeadsAmount, int[] programmersAmount)
+    {
+        teamLeads = new List<TeamLead>();
+        for (int i = 0; i < teamLeadsAmount; i++)
+        {
+            List<Programmer> programmers = new List<Programmer>();
+            for (int j = 0; j < programmersAmount[i]; j++)
+            {
+                programmers.Add(new Programmer(j + 1));
+            }
+            teamLeads.Add(new TeamLead(i + 1, programmers));
+        }
+    }
+
+    public List<TeamLead> TeamLeads
+    {
+        get { return teamLeads; }
+    }
+
+    public void PrintTeams()
+    {
+        for (int i = 0; i < teamLeads.Count; i++)
+        {
+            Console.WriteLine(teamLeads[i]);
+        }
+    }
+}
---
+++ classwork/TeamLead.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TeamLead : Programmer
+{
+    private readonly List<Programmer> programmers;
+
+    public TeamLead(int id, List<Programmer> programmers) : base(id)
+    {
+        this.programmers = programmers;
+    }
+
+    public List<Programmer> Programmers
+    {
+        get { return programmers; }
+    }
+
+    public void HuntProgrammers(List<TeamLead> teamLeads)
+    {
+        foreach(var teamLead in teamLeads)
+        {
+            for(int i = 0; i < teamLead.Programmers.Count; i++)
+            {
+                if(teamLead.Programmers[i].LinesOfCode % this.Id == 0)
+                {
+                    teamLead.Programmers.Remove(teamLead.Programmers[i]);
+                    this.Programmers.Add(teamLead.Programmers[i]);
+                }
+            }
+        }
+    }
+
+    public int GetWrittenLinesOfCode()
+    {
+        int linesOfCode = 0;
+        foreach(var programmer in programmers)
+        {
+            linesOfCode += programmer.LinesOfCode;
+        }
+        linesOfCode += this.LinesOfCode;
+        return linesOfCode;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Team lead #{0}\nAmount of programmers in team: {1}", Id, programmers.Count);
+    }
+}
